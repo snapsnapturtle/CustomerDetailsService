d2a99b0b6f80664cfe6b50e963160459d1d43c44
Jonah Möller <jonah.moeller@free-now.com>
2021-10-14 19:53:41 +0200

    feat: run spring init

---
+++ src/main/kotlin/ml/jonah/customerdetailsservice/CustomerDetailsServiceApplication.kt
@@ -0,0 +1,11 @@
+package ml.jonah.customerdetailsservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CustomerDetailsServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<CustomerDetailsServiceApplication>(*args)
+}
---
+++ src/test/kotlin/ml/jonah/customerdetailsservice/CustomerDetailsServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package ml.jonah.customerdetailsservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CustomerDetailsServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
