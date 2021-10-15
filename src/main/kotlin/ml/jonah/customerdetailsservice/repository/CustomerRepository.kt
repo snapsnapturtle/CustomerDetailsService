3ba776e758e32e9f5649d48b244c818e3cb85781
Jonah Möller <jonah.moeller@free-now.com>
2021-10-15 11:46:27 +0200

    feat: downgrade from JpaRepository to PagingAndSorting
    the additional featues from the JpaRepository are not used in this service, only the paging functionality is used.

--- src/main/kotlin/ml/jonah/customerdetailsservice/repository/CustomerRepository.kt
+++ src/main/kotlin/ml/jonah/customerdetailsservice/repository/CustomerRepository.kt
@@ -1,10 +1,9 @@
 package ml.jonah.customerdetailsservice.repository
 
 import ml.jonah.customerdetailsservice.entity.CustomerEntity
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.PagingAndSortingRepository
 import org.springframework.stereotype.Repository
 import java.util.UUID
 
 @Repository
-interface CustomerRepository : JpaRepository<CustomerEntity, UUID>
+interface CustomerRepository : PagingAndSortingRepository<CustomerEntity, UUID>
